a17d584dd1773d09a2036c4294af86d1b6385148
Jiu XU <twtygqyy@gmail.com>
2017-03-13 13:08:48 +0900

    update with new pytorch grad clip function

--- main.py
+++ main.py
@@ -69,25 +69,7 @@ def main():
     print("===> Training")
     for epoch in range(opt.start_epoch, opt.nEpochs + 1):        
         train(training_data_loader, optimizer, model, criterion, epoch)
-        save_checkpoint(model, epoch)
-
-def clip_grad_norm(parameters, max_norm, norm_type=2):
-    parameters = list(parameters)
-    max_norm = float(max_norm)
-    norm_type = float(norm_type)
-    if norm_type == float("inf"):
-        total_norm = max(p.grad.data.abs().max() for p in parameters)
-    else:
-        total_norm = 0
-        for p in parameters:
-            param_norm = p.grad.data.norm(norm_type)
-            total_norm += param_norm ** norm_type
-        total_norm = total_norm ** (1. / norm_type)
-    clip_coef = max_norm / (total_norm + 1e-6)
-    if clip_coef >= 1:
-        return
-    for p in parameters:
-        p.grad.data.mul_(clip_coef)        
+        save_checkpoint(model, epoch)    
     
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 10 epochs"""
@@ -114,7 +96,7 @@ def train(training_data_loader, optimizer, model, criterion, epoch):
         loss = criterion(model(input), target)         
         optimizer.zero_grad()        
         loss.backward() 
-        clip_grad_norm(model.parameters(),opt.clip) 
+        nn.utils.clip_grad_norm(model.parameters(),opt.clip) 
         optimizer.step()
         
         if iteration%100 == 0:
